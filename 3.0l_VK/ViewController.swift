f995a77101249e613dafa97adabe84c0f4f76648
iatman <DgamFX@mail.ru>
2019-06-30 01:40:38 +0300

    Initial Commit

---
+++ 3.0l_VK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  3.0l_VK
+//
+//  Created by Денис Баринов on 30/06/2019.
+//  Copyright © 2019 Денис Баринов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
